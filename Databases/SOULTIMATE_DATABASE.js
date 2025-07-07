dee9c5a526a24339db61b3771a90e7408abcf18d
Hexatunes <im.ramennoodles@gmail.com>
2025-07-07 11:06:21 -0400

    Setup

---
+++ Databases/SOULTIMATE_DATABASE.js
@@ -0,0 +1,13 @@
+const SOULTIMATE_DATABASE = {
+  "bonitoBlade": {
+    displayName: "Bonito Blade",
+    bp: 25,
+    hits: 10,
+    targets: "allEnemy",
+    element: "none",
+    type: "damage",
+    video: "./videos/battle/soultimateVideos/bonitoBlade.mp4"
+  },
+}
+
+module.exports = SOULTIMATE_DATABASE
